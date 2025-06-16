d9ecd30d1f075dbf23cd1f1f57c10142ecf37fbd
SavadaRamakrishna <147604512+SavadaRamakrishna@users.noreply.github.com>
2025-06-16 09:18:52 +0000

    update

--- practise/break-continue.sh
+++ practise/break-continue.sh
@@ -1,8 +1,1 @@
 #! /usr/bin/bash
-for i in {1..20}
-do 
- if [ $i -eq 15 ]; then
-   break
- fi
-   echo "number is $i"
-done
---
+++ practise/for-looping.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/bash
+list=("kittu" "chaitu" "baalu" "kallu" "ishu" "charitha" "raju" "rani")
+for frds in ${list[2]}
+do 
+  echo "he is my friend" $frds
+done
+
+for num in {1..20}
+do
+ echo "value is :"$num
+done
+
+for i in {1..20}
+do
+  echo $(( i/1 ))
+done
+
